ff3111daadc3a877ab884e1eca7115cb9f6759e0
RanjitVarun <ranjit.gaudaman@aspiresys.com>
2019-10-16 18:26:00 +0530

    route added and config vars

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ var http = require('http');
 var server = http.Server(app);
 
 app.use(express.static('client'));
+app.get('/route', (req, res) => res.send(showTimes()))
 
 server.listen(PORT, function() {
   console.log('Chat server running');
 });
 
-var io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
-});
+showTimes = () => {
+  let result = ''
+  const times = process.env.TIMES || 5
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  return result;
+}
