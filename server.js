457ab2f477aa94c01a419cf160c02ac4b6277cb5
RanjitVarun <ranjit.gaudaman@aspiresys.com>
2019-10-17 12:31:28 +0530

    postgres db added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
+const { Pool } = require('pg');
 
 var http = require('http');
 var server = http.Server(app);
@@ -9,8 +10,29 @@ app.use(express.static('client'));
 app.get('/route', (req, res) => res.send(showTimes()))
 
 server.listen(PORT, function() {
-  console.log('Chat server running');
+  console.log('server running');
 });
+
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+
 
 showTimes = () => {
   let result = ''
